85c42edb6842e8932460e519badc4438885950cd
Sarup Banskota <sbanskota08@gmail.com>
2016-04-24 18:48:03 +0800

    Introduce editable status property on leaves

--- app/routes/leaves/leave.js
+++ app/routes/leaves/leave.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	actions: {
+		updateLeaveStatus(leave, status){
+			leave.set('status', status);
+		}
+	},
 	model(params){
 		const record = this.get('record');
 		return record.getLeaveById(params.leave_id);
--- app/services/record.js
+++ app/services/record.js
@@ -9,21 +9,24 @@ const leaves = [
 		employee: "sarup.banskota@mesitis.com",
 		from: "12-12-2014",
 		to: "14-12-2014",
-		reason: "Need to shampoo my hair"
+		reason: "Need to shampoo my hair",
+		status: "Approved"
 	}),
 	Leave.create({
 		id: 2,
 		employee: "martin.pickrodt@mesitis.com",
 		from: "14-12-2015",
 		to: "16-12-2015",
-		reason: "Cat ate my pajamas"
+		reason: "Cat ate my pajamas",
+		status: "Approved"
 	}),
 	Leave.create({
 		id: 3,
 		employee: "tanmai.sharma@mesitis.com",
 		from: "2-1-2016",
 		to: "6-1-2016",
-		reason: "huh?"
+		reason: "huh?",
+		status: "Rejected"
 	})
 ];
 
